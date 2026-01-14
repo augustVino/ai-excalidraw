7bdfffa343b4b3905d5d5cdd03d68fe37a281a04
Vino <zhangyang1@liepin.com>
2026-01-14 20:02:27 +0800

    fix: enhance Vite configuration with API proxy and update fetch URL for chat completions

--- api/proxy.js
+++ api/proxy.js
@@ -5,7 +5,7 @@ module.exports = (req, res) => {
   let target = '';
   // 代理目标地址
   // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
-  if (req.url.startsWith('https://integrate.api.nvidia.com/v1')) {
+  if (req.url.startsWith('/api')) {
     //这里使用/api可能会与vercel serverless 的 api 路径冲突，根据接口进行调整
     target = 'https://integrate.api.nvidia.com/v1'; //这里就是在vite中配置的一样
   }
@@ -15,7 +15,7 @@ module.exports = (req, res) => {
     changeOrigin: true,
     pathRewrite: {
       // 通过路径重写，去除请求路径中的 `/api`
-      '^https://integrate.api.nvidia.com/v1/': '/'
+      '^/api/': '/'
     }
   })(req, res);
 };
